bc99249cae4971d5166a04d5c412f8ef21aa6c64
N1kkIRangel <carolinenicolle98@gmail.com>
2026-03-26 02:08:31 -0300

    Modifica algumas coisinhas e to ficando maluca ja

--- js/script_missing.js
+++ js/script_missing.js
@@ -1,42 +1,54 @@
 const form = document.getElementById("cadastrar");
 
 form.addEventListener("submit", function(event) {
-	
     event.preventDefault(); 
 
     const nome = document.getElementById("nome").value;
     const raca = document.getElementById("raca").value;
     const cor = document.getElementById("cor").value;
-    const sexo = document.querySelector('input[name="sexo"]:checked')?.value; //explicar
+    const sexo = document.querySelector('input[name="sexo"]:checked')?.value;
     const idade = document.getElementById("idade").value;
     const descricao = document.getElementById("descricao").value;
     const data = document.getElementById("data").value;
     const email = document.getElementById("email").value;
     const telefone = document.getElementById("telefone").value;
 
-    const foto = document.getElementById("foto").files[0]; //explicar
+    const foto = document.getElementById("foto").files[0];
+    const reader = new FileReader();
 
-    const reader = new FileReader(); 
-    reader.onload = function () {
-        
-        localStorage.setItem("nome", nome);
-        localStorage.setItem("raca", raca);
-        localStorage.setItem("cor", cor);
-        localStorage.setItem("sexo", sexo);
-        localStorage.setItem("idade", idade);
-        localStorage.setItem("descricao", descricao);
-        localStorage.setItem("data", data);
-        localStorage.setItem("email", email);
-        localStorage.setItem("telefone", telefone);
-        localStorage.setItem("foto", reader.result);
-	
-	if (foto) {
-    reader.readAsDataURL(foto); 
-	}
-    };
-	
-	window.location.href="desaparecidos.html";
+    if (foto) {
+        reader.onload = function () {
 
+            
+            sessionStorage.setItem("nome", nome);
+            sessionStorage.setItem("raca", raca);
+            sessionStorage.setItem("cor", cor);
+            sessionStorage.setItem("sexo", sexo);
+            sessionStorage.setItem("idade", idade);
+            sessionStorage.setItem("descricao", descricao);
+            sessionStorage.setItem("data", data);
+            sessionStorage.setItem("email", email);
+            sessionStorage.setItem("telefone", telefone);
+            sessionStorage.setItem("foto", reader.result);
 
+            
+            window.location.href = "desaparecidos.html";
+        };
+
+        reader.readAsDataURL(foto);
+
+    } else {
+
+        sessionStorage.setItem("nome", nome);
+        sessionStorage.setItem("raca", raca);
+        sessionStorage.setItem("cor", cor);
+        sessionStorage.setItem("sexo", sexo);
+        sessionStorage.setItem("idade", idade);
+        sessionStorage.setItem("descricao", descricao);
+        sessionStorage.setItem("data", data);
+        sessionStorage.setItem("email", email);
+        sessionStorage.setItem("telefone", telefone);
+
+        window.location.href = "desaparecidos.html";
+    }
 });
-		
