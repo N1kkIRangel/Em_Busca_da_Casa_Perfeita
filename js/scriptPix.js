edcbc0cf979a33cafeb2a6d575e6a76c3bd4b167
N1kkIRangel <carolinenicolle98@gmail.com>
2026-03-26 11:35:05 -0300

    Ajustes

--- js/scriptPix.js
+++
@@ -1,5 +0,0 @@
-function copiarPix(){
-	const pix = document.getElementById("pixkey").innerText;
-	navigator.clipboard.writeText(pix);
-	alert("Chave Pix copiada!");
-}
--- js/script_missing.js
+++ js/script_missing.js
@@ -1,54 +1,22 @@
 const form = document.getElementById("cadastrar");
 
 form.addEventListener("submit", function(event) {
-    event.preventDefault(); 
+    event.preventDefault();
 
-    const nome = document.getElementById("nome").value;
-    const raca = document.getElementById("raca").value;
-    const cor = document.getElementById("cor").value;
-    const sexo = document.querySelector('input[name="sexo"]:checked')?.value;
-    const idade = document.getElementById("idade").value;
-    const descricao = document.getElementById("descricao").value;
-    const data = document.getElementById("data").value;
-    const email = document.getElementById("email").value;
-    const telefone = document.getElementById("telefone").value;
-
-    const foto = document.getElementById("foto").files[0];
-    const reader = new FileReader();
-
-    if (foto) {
-        reader.onload = function () {
-
-            
-            sessionStorage.setItem("nome", nome);
-            sessionStorage.setItem("raca", raca);
-            sessionStorage.setItem("cor", cor);
-            sessionStorage.setItem("sexo", sexo);
-            sessionStorage.setItem("idade", idade);
-            sessionStorage.setItem("descricao", descricao);
-            sessionStorage.setItem("data", data);
-            sessionStorage.setItem("email", email);
-            sessionStorage.setItem("telefone", telefone);
-            sessionStorage.setItem("foto", reader.result);
-
-            
-            window.location.href = "desaparecidos.html";
-        };
-
-        reader.readAsDataURL(foto);
-
-    } else {
-
-        sessionStorage.setItem("nome", nome);
-        sessionStorage.setItem("raca", raca);
-        sessionStorage.setItem("cor", cor);
-        sessionStorage.setItem("sexo", sexo);
-        sessionStorage.setItem("idade", idade);
-        sessionStorage.setItem("descricao", descricao);
-        sessionStorage.setItem("data", data);
-        sessionStorage.setItem("email", email);
-        sessionStorage.setItem("telefone", telefone);
-
-        window.location.href = "desaparecidos.html";
-    }
+    const pet = {
+        nome: document.getElementById("nome").value,
+        raca: document.getElementById("raca").value,
+        cor: document.getElementById("cor").value,
+        idade: document.getElementById("idade").value,
+        descricao: document.getElementById("descricao").value,
+        data: document.getElementById("data").value,
+        email: document.getElementById("email").value,
+        telefone: document.getElementById("telefone").value,
+        sexo: document.querySelector('input[name="sexo"]:checked')?.value
+    };
+	
+	//salva temporariamente
+    sessionStorage.setItem("pet", JSON.stringify(pet));
+   
+    window.location.href = "desaparecidos.html";
 });
